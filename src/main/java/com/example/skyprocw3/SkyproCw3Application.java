87c81302b03b3469f445c5f21ea205ab3b6b4b22
Homework3 <tradervdovin@gmail.com>
2023-08-14 09:53:37 +0500

    1

---
+++ src/main/java/com/example/skyprocw3/SkyproCw3Application.java
@@ -0,0 +1,13 @@
+package com.example.skyprocw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCw3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skyprocw3/SkyproCw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprocw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
